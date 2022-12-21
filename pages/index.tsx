3e83412508dac49e294a020ce159e33d5de5b145
Stefano Consonni <stefanoconsonni@ymail.com>
2022-12-21 21:16:22 +0100

    create Sidebar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,42 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <Head>
-      <title>Twitter</title>
-      <meta name="description" content="Twitter" />
+    <>
+      <Head>
+        <title>Twitter</title>
+        <meta name="description" content="Twitter" />
 
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-    </Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      </Head>
+      <main>
+        {/* Sidebar */}
+
+        {/* Feed */}
+
+        {/* Widgets */}
+      </main>
+    </>
   );
 };
 
