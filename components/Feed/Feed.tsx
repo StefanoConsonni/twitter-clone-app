e1938282bc8ef42ec878a90892f0ecca1138f7ba
Stefano Consonni <stefanoconsonni@ymail.com>
2023-01-29 20:13:06 +0100

    create Tweet component

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+import { default as axios, AxiosResponse } from "axios";
+
 import styles from "./styles.module.css";
 import SyncIcon from "@mui/icons-material/Sync";
 import TweetBox from "../TweetBox/TweetBox";
+import Tweet from "../Tweet/Tweet";
+import { ITweet } from "../../interfaces";
 
 const iconStyles = {
   color: "var(--light-blue)",
@@ -20,14 +25,39 @@ const iconStyles = {
 };
 
 function Feed() {
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["tweets"],
+    queryFn: () => {
+      return axios
+        .get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`)
+        .then((res: AxiosResponse) => res.data);
+    },
+  });
+  console.log(data);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (isError) {
+    return <h1>Something went wrong</h1>;
+  }
+
   return (
     <div className={styles["feed-container"]}>
       <div className={styles["header-container"]}>
         <h1 className={styles["feed-title"]}>Home</h1>
-        <SyncIcon sx={iconStyles} />
+        <SyncIcon sx={iconStyles} onClick={() => refetch()} />
       </div>
       <div>
         <TweetBox />
+      </div>
+
+      {/* Feed */}
+      <div>
+        {data.map((tweet: ITweet) => (
+          <Tweet key={tweet._id} tweet={tweet} />
+        ))}
       </div>
     </div>
   );
---
+++ components/Tweet/Tweet.tsx
@@ -0,0 +1,11 @@
+import { ITweet } from "../../interfaces";
+
+function Tweet({ tweet }: ITweet) {
+  return (
+    <div>
+      <h1>tweet</h1>
+    </div>
+  );
+}
+
+export default Tweet;
